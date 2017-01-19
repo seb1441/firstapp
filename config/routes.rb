7153d0ac31219634f76e4b55199e5bb2a22b3e18
Seb <sebastienauriault@gmail.com>
2017-01-18 20:28:27 -0500

    3.11 added working links to menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  # get 'static_pages/index'
   root 'static_pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
