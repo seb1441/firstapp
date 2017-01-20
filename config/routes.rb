91e5f9a130fd0854d6754625880f9b03f3605bf8
Seb <sebastienauriault@gmail.com>
2017-01-20 08:18:50 -0500

    4.3 added orders controller and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :products
-  root 'static_pages#index'
+  resources :orders, only: [:index, :show, :create, :destroy]
+  # root 'static_pages#index'
+  root 'static_pages#landing_page'
+  get 'static_pages/index'
   get 'static_pages/about'
   get 'static_pages/contact'
 end
