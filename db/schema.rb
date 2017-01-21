da3d34fa4a9bfe00dfb7e38ce5df53d42d9919d9
Seb <sebastienauriault@gmail.com>
2017-01-21 00:17:34 -0500

    4.2 changed color attribute to colour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120111848) do
+ActiveRecord::Schema.define(version: 20170121051036) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170120111848) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
+    t.string   "colour"
     t.integer  "price"
   end
 
