d3ffeef5428e6b3948ffb95b11aeeb0c9598f2aa
Seb <sebastienauriault@gmail.com>
2017-01-26 07:39:00 -0500

    5.1 rerouted the root page and added dynamic links to the nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  # root 'static_pages#index'
-  root 'static_pages#landing_page'
+
   get '/home', to: 'static_pages#index'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  # Changes for task 5.1
+  #root 'static_pages#landing_page'
+  root 'products#index'
 end
