aa6dad3f53d9b43ba8095fdb276692ca137a314c
Seb <sebastienauriault@gmail.com>
2017-01-25 22:38:26 -0500

    5.1 added product validations and changed routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_many :orders
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :colour, presence: true
+  validates :price, presence: true,
+                    numericality: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   # root 'static_pages#index'
   root 'static_pages#landing_page'
-  get 'static_pages/index'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/home', to: 'static_pages#index'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
