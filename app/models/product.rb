cc31279e43699059f8e31e113ff1e50b7cca95de
seb1441 <sebastienauriault@gmail.com>
2017-02-28 20:02:03 -0500

    5.7 added comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,18 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
   validates :name, presence: true
   validates :description, presence: true
   validates :image_url, presence: true
   validates :colour, presence: true
   validates :price, presence: true,
-                    numericality: true
+                numericality: true
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
 end
