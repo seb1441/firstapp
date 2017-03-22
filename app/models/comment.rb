471966d64fa671dd93ea704351c85743dd5a79eb
seb1441 <sebastienauriault@gmail.com>
2017-03-22 19:20:09 -0400

    5.9 add comment body validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,12 @@ class Comment < ApplicationRecord
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
+  validates :body, presence: true
 
   belongs_to :user
   belongs_to :product
 
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
-  
+
 end
