a94489fe244350c162a1de0a52b6886bec1afc52
seb1441 <sebastienauriault@gmail.com>
2017-03-23 10:17:03 -0400

    5.8 second attempt to fix production images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  
+  #Fix rating stars images
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
