dd8659e2e0021da67884cd017d0b47e5c4e9aaab
seb1441 <sebastienauriault@gmail.com>
2017-03-30 20:11:19 -0400

    5.8 fix images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  #TEST CHANGE THIS LINE TO FIX IMAGES
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -27,7 +30,6 @@ Rails.application.configure do
 
   #Fix rating stars images
   config.assets.digest = true
-  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
