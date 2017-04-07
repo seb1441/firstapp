e075daba8d6537a4c61e65521c0e3d5e28a5994c
seb1441 <sebastienauriault@gmail.com>
2017-04-07 19:00:31 -0400

    5.11 add auto email registration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,11 @@ class UserMailer < ApplicationMailer
         :to => 'sebastienauriault@gmail.com',
         :subject => "A new contact form message from #{first_name} #{last_name}")
   end
+
+  def welcome(user)
+    @appname = "Bike Shop"
+    mail( :to => user.email,
+          :subject => "Welcome to #{@appname}!")
+  end
+
 end
