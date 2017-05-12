6a6d5fec9eb362bcd14dc07695323335a8a8e8a5
seb1441 <sebastienauriault@gmail.com>
2017-05-11 21:01:13 -0400

    6.7 byebug gem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
+    byebug
     if params[:q]
       search_term = params[:q]
       if Rails.env == "production"
