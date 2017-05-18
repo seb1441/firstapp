a9eda2b63b46ad17e692f81e8166766c8a5f52e3
seb1441 <sebastienauriault@gmail.com>
2017-05-18 11:22:19 -0400

    6.6 change routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'payments/create'
+  post 'payments/create'
 
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations" }
 
